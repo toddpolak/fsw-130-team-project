a2175aff736385d1a3699dfea6e579e7dee61548
toddpolak <todd.polak@bryanuniversity.edu>
2021-01-28 12:52:23 -0800

    'Todd-Branch'

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -6,7 +6,7 @@ function Movies() {
             <h1>Movies Page</h1>
             Project 3: Movies & TV with Redux (React)
             <p>Form here to add and delete movies</p>
-            <P>(Display movies here)</P>
+            <p>(Display movies here)</p>
         </div>
     )
 }
--- src/pages/TvShows.js
+++ src/pages/TvShows.js
@@ -6,7 +6,7 @@ function TvShows() {
             <h1>TV Shows Page</h1>
             Project 3: Movies & TV with Redux (React)
             <p>Form here to add and delete tv shows</p>
-            <P>(Display tv shows here)</P>
+            <p>(Display tv shows here)</p>
         </div>
     )
 }
